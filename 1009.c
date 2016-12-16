8ea123da55f4439aae406257d1e236bfbd784752
anyaliang <anyalian@outlook.com>
2016-12-16 23:35:22 +0800

    create

---
+++ 1009.c
@@ -0,0 +1,15 @@
+//here I come --> come I here
+
+#include <stdio.h>
+
+int main(void){
+    char input[80] = {0};
+    int count = 0;
+
+    scanf("%c",input[count]);
+    while(count != '\n'){
+    scanf("%c",input[count]);
+    }
+
+    return 0;
+}
--- 1023.c
+++ 1023.c
@@ -3,41 +3,31 @@
 int main(void){
     int int_digit[10] = {0}; //10 is [0-9] use the value to count times
     int count = 1;//count for min except 0 
-    char char_input;
-    int int_input = 0;
+    int input = 0;
     int i = 0;
     int j = 0;
+    int m = 0;
+    int no = 0;
 
-    scanf("%c", &char_input);
-    while(char_input != '\n'){
-        int_input = (int)(char_input -48);
-        int_digit[int_input] += 1;
-        scanf("%c", &char_input);
-    } //transfer into int and record times by array
+    while(no < 10){
+        scanf("%d", &input);
+        int_digit[no++] = input;//int_digit[no] for num of [1-9]
+    } 
 
     while(int_digit[count] == 0){
-        count += 1;
-    }//count now is the index of the min nicht 0 
-
-    for(i = 0; i < int_digit[count]; i++){
-        printf("%d", count);
-    }//print min nicht 0
-
-    if(int_digit[0]){
-        for(i = 0; i < int_digit[0]; i++){
-            printf("%d", 0);
-        }//print 0 if have
+        count++;//min nicht 0
     }
 
-    for(i = count + 1; i < 10; i++){
-        if(int_digit[i]){
-            for(j = 0; j < int_digit[i]; j++){
-                printf("%d", i);
+    printf("%d", count);
+    int_digit[count]--;
+
+    while(j < 10){
+        if(int_digit[j]){
+            for(m = 0; m < int_digit[j]; m++){
+                printf("%d", j);
             }
         }
+        j++;
     }
-
-  //  putchar('\0');
-
     return 0;
-        }
+} 
